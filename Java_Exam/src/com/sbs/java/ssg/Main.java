f0a6ee14b89982cd0a5ca6fa1cff512d0ffaf039
woooyoung <axdsw121@gmail.com>
2021-04-06 02:04:10 +0900

    게시물 작성기능

--- Java_Exam/src/com/sbs/java/ssg/Main.java
+++ Java_Exam/src/com/sbs/java/ssg/Main.java
@@ -7,15 +7,39 @@ public class Main {
 		System.out.println("== 프로그램 시작 ==");
 
 		Scanner sc = new Scanner(System.in);
-		String command;
+		
+		int lastArticleId = 0;
+		
+		while (true) {
+			System.out.printf("명령어) ");
+			String command = sc.nextLine();
 
-		System.out.printf("명령어) ");
-		command = sc.next();
-		System.out.printf("입력된 명령어 : %s\n", command);
+			command = command.trim();
 
-		System.out.printf("명령어) ");
-		int num = sc.nextInt();
-		System.out.printf("입력된 숫자 : %d\n", num);
+			if (command.length() == 0) {
+				continue;
+			}
+
+			if (command.equals("system exit")) {
+				break;
+			}
+			if (command.equals("article write")) {
+				int id = lastArticleId + 1;
+				lastArticleId = id;
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+
+				System.out.printf("%d번글이 생성되었습니다.\n", id);
+
+			} else if (command.equals("article list")) {
+				System.out.println("게시물이 없습니다.");
+			} else {
+				System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n", command);
+			}
+		}
+
 		sc.close();
 
 		System.out.println("== 프로그램 끝 ==");
