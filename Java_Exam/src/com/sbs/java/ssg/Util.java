4b91884fad0b05267e1c5ff5ee970068a0906d02
woooyoung <axdsw121@gmail.com>
2021-04-07 00:48:03 +0900

    게시물 저장시 작성날짜 저장

---
+++ Java_Exam/src/com/sbs/java/ssg/Util.java
@@ -0,0 +1,12 @@
+package com.sbs.java.ssg;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Util {
+	public static String getNowDateStr() {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date time = new Date();
+		return format.format(time);
+	}
+}
