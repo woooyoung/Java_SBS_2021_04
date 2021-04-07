a79689845266b97a97b6bef6478b47071bbf94cb
woooyoung <axdsw121@gmail.com>
2021-04-08 02:39:45 +0900

    테스트 데이터 생성 메서드 구현하고, 프로그램 시작시에 적용

--- Java_Exam/src/com/sbs/java/ssg/Main.java
+++ Java_Exam/src/com/sbs/java/ssg/Main.java
@@ -5,14 +5,18 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+	private static List<Article> articles;
+
+	static {
+		articles = new ArrayList<>();
+	}
+
 	public static void main(String[] args) {
 		System.out.println("== 프로그램 시작 ==");
 
+		makeTestData();
+
 		Scanner sc = new Scanner(System.in);
-
-		int lastArticleId = 0;
-
-		List<Article> articles = new ArrayList<>();
 
 		while (true) {
 			System.out.printf("명령어) ");
@@ -29,8 +33,7 @@ public class Main {
 			}
 
 			if (command.equals("article write")) {
-				int id = lastArticleId + 1;
-				lastArticleId = id;
+				int id = articles.size() + 1;
 				String regDate = Util.getNowDateStr();
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
@@ -72,7 +75,7 @@ public class Main {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
 					continue;
 				}
-				
+
 				foundArticle.increaseHit();
 
 				System.out.printf("번호 : %d\n", foundArticle.id);
@@ -141,6 +144,14 @@ public class Main {
 
 		System.out.println("== 프로그램 끝 ==");
 	}
+
+	private static void makeTestData() {
+		System.out.println("테스트를 위한 데이터를 생성합니다.");
+		
+		articles.add(new Article(1, Util.getNowDateStr(), "제목1", "내용1", 10));
+		articles.add(new Article(2, Util.getNowDateStr(), "제목2", "내용2", 22));
+		articles.add(new Article(3, Util.getNowDateStr(), "제목3", "내용3", 33));
+	}
 }
 
 class Article {
@@ -149,15 +160,19 @@ class Article {
 	String title;
 	String body;
 	int hit;
-
+	
 	public Article(int id, String regDate, String title, String body) {
+		this(id, regDate, title, body, 0);
+	}
+	
+	public Article(int id, String regDate, String title, String body, int hit) {
 		this.id = id;
 		this.regDate = regDate;
 		this.title = title;
 		this.body = body;
-		this.hit = 0;
+		this.hit = hit;
 	}
-	
+
 	public void increaseHit() {
 		hit++;
 	}
