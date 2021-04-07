6489d061cacfcd493a327a49b5e3287008d9d270
woooyoung <axdsw121@gmail.com>
2021-04-08 02:42:34 +0900

    리펙토링, 클래스를 파일로 분리, 패키지 분리, App 클래스로 핵심로직 이전

---
+++ Java_Exam/src/com/sbs/java/ssg/util/Util.java
@@ -0,0 +1,12 @@
+package com.sbs.java.ssg.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Util {
+	public static String getNowDateStr() {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date time = new Date();
+		return format.format(time);
+	}
+}
