814eac3f13544e7f1ad9930ff7b142478dddcc8e
woooyoung <axdsw121@gmail.com>
2021-04-09 02:11:57 +0900

    MemberController(창구 직원) 도입

--- Java_Exam/src/com/sbs/java/ssg/App.java
+++ Java_Exam/src/com/sbs/java/ssg/App.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import com.sbs.java.ssg.controller.MemberController;
 import com.sbs.java.ssg.dto.Article;
 import com.sbs.java.ssg.dto.Member;
 import com.sbs.java.ssg.util.Util;
@@ -24,6 +25,8 @@ public class App {
 
 		Scanner sc = new Scanner(System.in);
 
+		MemberController memberController = new MemberController(sc, members);
+
 		while (true) {
 			System.out.printf("명령어) ");
 			String command = sc.nextLine();
@@ -39,48 +42,7 @@ public class App {
 			}
 
 			if (command.equals("member join")) {
-				int id = members.size() + 1;
-				String regDate = Util.getNowDateStr();
-
-				String loginId = null;
-
-				while (true) {
-					System.out.printf("로그인 아이디 : ");
-					loginId = sc.nextLine();
-
-					if (isJoinableLoginId(loginId) == false) {
-						System.out.printf("%s(은)는 이미 사용중인 아이디 입니다.\n", loginId);
-						continue;
-					}
-
-					break;
-				}
-
-				String loginPw = null;
-				String loginPwConfirm = null;
-
-				while (true) {
-					System.out.printf("로그인 비번 : ");
-					loginPw = sc.nextLine();
-					System.out.printf("로그인 비번확인 : ");
-					loginPwConfirm = sc.nextLine();
-
-					if (loginPw.equals(loginPwConfirm) == false) {
-						System.out.println("비밀번호를 다시 입력해주세요.");
-						continue;
-					}
-
-					break;
-				}
-
-				System.out.printf("이름 : ");
-				String name = sc.nextLine();
-
-				Member member = new Member(id, regDate, loginId, loginPw, name);
-				members.add(member);
-
-				System.out.printf("%d번 회원이 생성되었습니다. 환영합니다.\n", id);
-
+				memberController.doJoin();
 			} else if (command.equals("article write")) {
 				int id = articles.size() + 1;
 				String regDate = Util.getNowDateStr();
@@ -185,29 +147,6 @@ public class App {
 		sc.close();
 
 		System.out.println("== 프로그램 끝 ==");
-	}
-
-	private boolean isJoinableLoginId(String loginId) {
-		int index = getMemberIndexByLoginId(loginId);
-		
-		if ( index == -1 ) {
-			return true;
-		}
-		
-		return false;
-	}
-
-	private int getMemberIndexByLoginId(String loginId) {
-		int i = 0;
-		for ( Member member : members ) {
-			if ( member.loginId.equals(loginId) ) {
-				return i;
-			}
-			
-			i++;
-		}
-		
-		return -1;
 	}
 
 	private int getArticleIndexById(int id) {
